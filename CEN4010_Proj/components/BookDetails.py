927767ca0c6bae94fbb2e1995067a93553c737c8
Ernesto Leiva <eleiv009@fiu.edu>
2023-10-22 18:22:33 -0400

    updated bookdetails

--- CEN4010_Proj/components/BookDetails.py
+++ CEN4010_Proj/components/BookDetails.py
@@ -1,10 +1,10 @@
-#Ernesto
+#Ernesto Leiva
+from marshmallow import fields
 from __main__ import db, ma, app
 
-# Template class: [4] Book Details
-class Book(db.Model):
-    # Schema
-    class ProductSchema(ma.Schema):
+
+class BookSchema(ma.Schema):
+        price = fields.Float()
         class Meta:
             fields = (
                 "id",
@@ -20,6 +20,12 @@ class Book(db.Model):
                 "Rating",
             )
 
+# Product schema for single and multiple items
+book_schema = BookSchema()
+books_schema = BookSchema(many=True)
+
+# Template class: [4] Book Details
+class Book(db.Model):
     # Create DB fields
     id = db.Column(db.Integer, primary_key=True)
     ISBN = db.Column(db.Integer, unique=True)
@@ -32,10 +38,6 @@ class Book(db.Model):
     YearPublished = db.Column(db.Integer)
     Sold = db.Column(db.Integer)
     Rating = db.Column(db.Integer)
-
-    # Product schema for single and multiple items
-    product_schema = ProductSchema()
-    products_schema = ProductSchema(many=True)
 
     def __init__(
         self, ISBN, Name, Desc, Price, Auth, Genre, Pub, Year, Sold, Rating
@@ -50,3 +52,27 @@ class Book(db.Model):
         self.YearPublished = Year
         self.Sold = Sold
         self.Rating = Rating
+
+    @classmethod
+    def add_book(cls, isbn, name,description, genre, copies_sold, book_rating, price,publisher,author,year_published):
+        with app.app_context():
+            db.create_all()
+            existing_book = cls.query.filter_by(isbn=isbn).first()
+            if existing_book is None:
+                new_book = cls(isbn=isbn, name=name,description=description, genre=genre, copies_sold=copies_sold, book_rating=book_rating, price=price,publisher=publisher,author=author,year_published=year_published)
+                db.session.add(new_book)
+                db.session.commit()
+                return new_book
+            else:
+                return None  
+
+    @classmethod
+    def display_all_books(cls):
+        with app.app_context():
+            book_entries = cls.query.all()
+
+            if book_entries:
+             return books_schema.dumps(book_entries)
+            else:
+                return None
+        
