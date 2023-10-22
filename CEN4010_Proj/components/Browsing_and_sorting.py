bd5e8054b8d8f3b31da6d7e8a6e191ce8eaaa00a
Ernesto Leiva <eleiv009@fiu.edu>
2023-10-22 18:58:03 -0400

    updated sorting and browsing

--- CEN4010_Proj/components/Browsing_and_sorting.py
+++ CEN4010_Proj/components/Browsing_and_sorting.py
@@ -4,68 +4,19 @@ from marshmallow import fields
 from __main__ import db, ma, app
 from components.BookDetails import Book, books_schema
 
-class BookSchema(ma.Schema):
-    price = fields.Float()  
-    class Meta:
-    
-        fields = ("id","isbn", "name","description", "genre", "copies_sold", "book_rating", "price","publisher","author","year_published")
 
-#Initialize schema
-book_schema = BookSchema()
-books_schema = BookSchema(many=True)  # To handle multiple Book objects
-#db = SQLAlchemy(app)
-
-@classmethod
-def update_discount_book(cls, publisher,discount_percent):
-    with app.app_context():
-            affected_rows = Book.query.filter_by(publisher=publisher).update({
-            'price': Book.price - (Book.price * discount_percent / 100)
-    })
-    db.session.commit()
-    if affected_rows:
-        return affected_rows
-    else:
-        return None
-
-@classmethod
-def search_books_by_genre_JSON(cls, genre):
-    with app.app_context():
-        book_entries = cls.query.filter_by(genre=genre).all()
-
-    if book_entries:
-            return Book.dumps(book_entries)
-    else:
-        return None
-
-@classmethod
-def list_books_by_book_name_JSON(cls, name):
-    with app.app_context():
-        book_entries = cls.query.filter_by(name=name).all()
-
-    if book_entries:
-            return Book.dumps(book_entries)
-    else:
-        return None
-@classmethod
-def search_books_by_description_JSON(cls, description):
-    with app.app_context():
-        book_entries = cls.query.filter_by(description=description).all()
-
-    if book_entries:
-            return Book.dumps(book_entries)
-    else:
-        return None
-    
-@classmethod
-def search_top_ten_book_count_JSON(cls):
-    with app.app_context():
-        book_entries = cls.query.order_by(cls.copies_sold.desc()).limit(10).all()
-
-    if book_entries:
-            return Book.dumps(book_entries)
-    else:
+class Browsing_and_Sorting:
+    @classmethod
+    def update_discount_book(cls, publisher,discount_percent):
+        with app.app_context():
+                affected_rows = Book.query.filter_by(publisher=publisher).update({
+                'price': Book.price - (Book.price * discount_percent / 100)
+        })
+        db.session.commit()
+        if affected_rows:
+            return affected_rows
+        else:
             return None
-
 
     @classmethod
     def search_books_by_genre_JSON(cls, genre):
@@ -86,7 +37,6 @@ def search_top_ten_book_count_JSON(cls):
                 return Book.dumps(book_entries)
         else:
             return None
-
     @classmethod
     def search_books_by_description_JSON(cls, description):
         with app.app_context():
@@ -107,7 +57,6 @@ def search_top_ten_book_count_JSON(cls):
         else:
                 return None
 
-
     @classmethod
     def search_books_by_price_JSON(cls, price):
         with app.app_context():
@@ -118,21 +67,10 @@ def search_top_ten_book_count_JSON(cls):
         else:
             return None
 
-
     @classmethod
     def search_books_by_book_rating_JSON(cls, book_rating):
         with app.app_context():
                 book_entries = cls.query.filter(cls.book_rating >= book_rating).all()
-
-        if book_entries:
-                return Book.dumps(book_entries)
-        else:
-            return None
-
-    @classmethod
-    def search_books_by_genre_JSON(cls, genre):
-        with app.app_context():
-            book_entries = cls.query.filter_by(genre=genre).all()
 
         if book_entries:
                 return Book.dumps(book_entries)
@@ -148,7 +86,8 @@ def search_top_ten_book_count_JSON(cls):
                 return Book.dumps(book_entries)
         else:
             return None
-    
+
+
     """ --->>> Commented these two out as they were moved to BookDetails.py
     @classmethod
     def add_book(cls, isbn, name,description, genre, copies_sold, book_rating, price,publisher,author,year_published):
