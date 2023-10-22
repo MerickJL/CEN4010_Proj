baf6bcbab8d0bf99716ee2c11881deae589cf12e
Maverick Jude Lenon <143677915+MerickJL@users.noreply.github.com>
2023-10-22 00:32:16 -0400

    Update BookstoreAPI.py

--- CEN4010_Proj/BookstoreAPI.py
+++ CEN4010_Proj/BookstoreAPI.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 DBPATH = "sqlite:///" + os.path.join(
     os.path.abspath(os.path.dirname(__file__)), "db.sqlite"
 )
-
 # Set database
 app.config["SQLALCHEMY_DATABASE_URI"] = DBPATH
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -25,7 +24,6 @@ We are going to keep classes in separate files. This way it will be easier to
 maintain and integrate new features. Please be sure to name classes uniquely.
 """
 import components.views  # noqa:402
-# import components.views_browsing_and_sorting_sachin # noqa:402
 
 if __name__ == "__main__":
     with app.app_context():
