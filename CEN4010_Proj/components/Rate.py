a6c3731243b9cb352de0e55cec9088e678af511d
Maverick Jude Lenon <143677915+MerickJL@users.noreply.github.com>
2023-11-27 22:48:07 -0500

    Update Rate.py

--- CEN4010_Proj/components/Rate.py
+++ CEN4010_Proj/components/Rate.py
@@ -24,18 +24,3 @@ class Rate(db.Model):
         self.rating = rating
         self.comment = comment
         self.time = time
-
-# class Rating(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     value = db.Column(db.Integer, nullable=False)
-#     user_id = db.Column(db.Integer, nullable=False)
-#     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
-#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-# class Comment(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     text = db.Column(db.Text, nullable=False)
-#     user_id = db.Column(db.Integer, nullable=False)
-#     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
-#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
